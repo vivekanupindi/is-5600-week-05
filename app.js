805821f3e839bc3aebd2e5c77b2cbe001a380f44
vivekanupindi <vivek.anupindi@slu.edu>
2024-11-22 23:28:44 +0000

    Week 05 Commit Changes

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
+app.get('/orders', api.listOrders)
+app.get('/orders/', api.createOrder)
+app.put('/orders/:id', api.editOrder)   // Register the edit order route
+app.delete('/orders/:id', api.deleteOrder) // Register the delete order route
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
